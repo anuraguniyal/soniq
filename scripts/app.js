41e8df164d65e79b67382c514d1cc06b7f714a6a
Anurag Uniyal <anuraguniyal@gmail.com>
2019-10-04 14:34:26 -0700

    added audio listner with visualization

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,10 @@ var g_audioContext = null;
 var g_transmitting = false;
 var g_receiving = false;
 var g_oscillator = null
+var g_analyser = null;
+var g_dataArray = null;
+var g_width=null;
+var g_height=null;
 
 function getAudioContext(){
   if(g_audioContext != null)
@@ -12,12 +16,78 @@ function getAudioContext(){
   return g_audioContext
 }
 
-function startReceiver(){
+function getCanvasContext(){
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  g_width = canvas.width;
+  g_height = canvas.height;
+  return ctx;
 }
 
+function startReceiver(){
+  if(g_analyser != null){
+    captureAudio();
+    return
+  }
+
+  if (navigator.mediaDevices) {
+      console.log('getUserMedia supported.');
+      navigator.mediaDevices.getUserMedia ({audio: true, video: false})
+      .then(function(stream) {
+          // Create a MediaStreamAudioSourceNode
+          // Feed the HTMLMediaElement into it
+          var context = getAudioContext();
+          var source = context.createMediaStreamSource(stream);
+
+          g_analyser = context.createAnalyser();
+          g_analyser.connect(context.destination)
+          source.connect(g_analyser)
+
+          g_analyser.fftSize = 256;
+          var bufferLength = g_analyser.frequencyBinCount;
+          console.log(bufferLength);
+          g_dataArray = new Uint8Array(bufferLength);
+          captureAudio();
+      })
+      .catch(function(err) {
+          console.log('The following gUM error occured: ' + err);
+      });
+  } else {
+     console.log('getUserMedia not supported on your browser!');
+  }
+}
+
+function captureAudio(){
+  if(!g_receiving) return;
+
+  var ctx = getCanvasContext();
+  var drawVisual = window.requestAnimationFrame(captureAudio);
+  g_analyser.getByteFrequencyData(g_dataArray);
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(0, 0, g_width, g_height);
+
+  var barWidth = (g_width / g_dataArray.length) * 2.5;
+  var barHeight;
+  var x = 0;
+
+  for(var i = 0; i < g_dataArray.length; i++) {
+    barHeight = g_dataArray[i];
+
+    ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+    ctx.fillRect(x, g_height-barHeight/2,barWidth,barHeight/2);
+
+    x += barWidth + 1;
+  }
+}
+
+function stopReceiver(){
+  console.log("canvas", g_width, g_height);
+  var ctx = getCanvasContext();
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fillRect(0, 0, g_width, g_height);
+}
 
 function startTransmitter(){
-  g_transmitting = true ;
   var context = getAudioContext();
   if(g_oscillator == null){
     g_oscillator = context.createOscillator();
@@ -38,13 +108,26 @@ function stopTransmitter(){
 }
 
 function toggleReceiver(){
+  if(g_receiving){
+    g_receiving = false;
+    stopReceiver();
+    document.getElementById('btn_receiver').innerText = "Start Transmitter";
+  }else{
+    g_receiving = true;
+    startReceiver()
+    document.getElementById('btn_receiver').innerText = "Stop Transmitter";
+  }
 }
 
 function toggleTransmitter(){
   if(g_transmitting){
+    g_transmitting = false;
     stopTransmitter();
+    document.getElementById('btn_transmitter').innerText = "Start Transmitter";
   }else{
+    g_transmitting = true;
     startTransmitter()
+    document.getElementById('btn_transmitter').innerText = "Stop Transmitter";
   }
 }
 
